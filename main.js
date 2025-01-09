a53950119ad064c4e45f3e9129ef5e598ef9fcf8
KuroiSenpai <96829733+KuroiSenpai@users.noreply.github.com>
2025-01-09 22:55:55 +0100

    anpassungen an formular

--- main.js
+++ main.js
@@ -42,3 +42,35 @@ function showMotiv(motivID) {
         selectedMotiv.style.display = 'block';
     }
 }
+const outputRef = document.querySelector('.outputRef');
+const fileInputRef = document.querySelector("#referenzbilder");
+
+fileInputRef.addEventListener("change", () => {
+    for (const file of fileInputRef.files) {
+        outputRef.innerText += `\n${file.name}`;
+    }
+});
+
+const outputKoerper = document.querySelector('.outputKoerper');
+const fileInputKoerper = document.querySelector("#koerperstelle");
+fileInputKoerper.addEventListener("change", () => {
+    for (const file of fileInputKoerper.files) {
+        outputKoerper.innerText += `\n${file.name}`;
+    }
+});
+
+const outputWunsch = document.querySelector('.outputWunsch');
+const fileInputWunsch = document.querySelector("#wunschmotiv");
+fileInputWunsch.addEventListener("change", () => {
+    for (const file of fileInputWunsch.files) {
+        outputWunsch.innerText += `\n${file.name}`;
+    }
+});
+
+const outputBild = document.querySelector('.outputBild');
+const fileInputKoerper2 = document.querySelector("#koerperbilder");
+fileInputKoerper2.addEventListener("change", () => {
+    for (const file of fileInputKoerper2.files) {
+        outputBild.innerText += `\n${file.name}`;
+    }
+});
